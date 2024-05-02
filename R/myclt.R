0c93f1cfec23f072212c2b4f954f87bb273fc3a9
Blue_water_okie <54322474+okie-creator@users.noreply.github.com>
2024-05-02 11:25:01 -0500

    made some adjustments

--- R/myclt.R
+++ R/myclt.R
@@ -13,6 +13,6 @@ myclt=function(n = 100,iter = 1000,a=0,b=5){
   data=matrix(y,nr=n,nc=iter,byrow=TRUE)
   sm=apply(data,2,sum)
   h=hist(sm,plot=FALSE)
-  hist(sm,col=rainbow(length(h$mids)),freq=FALSE,main="Distribution of the sum of uniforms")
+  hist(sm,col=rainbow(length(h$mids)),freq=FALSE,main="CLT of sum of uniforms")
   curve(dnorm(x,mean=n*(a+b)/2,sd=sqrt(n*(b-a)^2/12)),add=TRUE,lwd=2,col="Blue")
 }
